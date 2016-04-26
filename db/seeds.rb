1f276caa443fb1b78e45580dc450e6f7015199f2
Hilary Masland <hilarymasland@gmail.com>
2016-04-26 18:40:25 -0400

    working on rendering seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,33 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 city = City.create({ cityname: 'Washington D.C.'})
+city = City.create({ cityname: 'New York City'})
+# city = City.create({ cityname: 'Baltimore'})
+# city = City.create({ cityname: 'San Diego'})
+# city = City.create({ cityname: 'Arlington'})
 
-city.shops.create({ shopname: 'Starbucks', properties: {
+city.shops.create({ shopname: 'Starbucks', location: 'Washington D.C.', properties: {
   'Outdoor Seating': true,
   'Wifi': true,
   'Soy Milk': true,
-  'Music': true,
+  'Music': false,
   }})
+
+city.shops.create({ shopname: 'Port City Java', location: 'Washington D.C.', properties: {
+    'Outdoor Seating': true,
+    'Wifi': true,
+    'Soy Milk': false,
+    'Music': true,
+    }})
+city.shops.create({ shopname: 'Bean & Bite', location: 'Washington D.C.', properties: {
+      'Outdoor Seating': true,
+      'Wifi': true,
+      'Soy Milk': true,
+      'Music': false,
+      }})
+city.shops.create({ shopname: 'Starbucks', location: 'Washington D.C.', properties: {
+        'Outdoor Seating': true,
+        'Wifi': true,
+        'Soy Milk': true,
+        'Music': true,
+        }})
